e0024942c998b5cbcb45cd5a87cbfbf773e19bb0
Sean Lyons <lyons.sean5@gmail.com>
2025-12-24 15:07:58 -0500

    Add RPC stubs for CASPaxos server

--- modules/replication/src/client/main.cc
+++ modules/replication/src/client/main.cc
@@ -1,71 +1,68 @@
-#include <iostream>
+#include <condition_variable>
+#include <mutex>
 #include <optional>
 #include <string>
 
+#include "absl/base/log_severity.h"
 #include "absl/flags/flag.h"
 #include "absl/flags/parse.h"
 #include "absl/flags/usage.h"
+#include "absl/log/check.h"
+#include "absl/log/globals.h"
+#include "absl/log/initialize.h"
+#include "absl/log/log.h"
 #include "grpc++/grpc++.h"
-#include "modules/replication/proto/foo.grpc.pb.h"
+#include "modules/replication/proto/caspaxos.grpc.pb.h"
 
 ABSL_FLAG(std::string, target, "localhost:50051", "Server address");
 ABSL_FLAG(std::optional<int32_t>, value, std::nullopt, "Value to set");
 
-using foo::Foo;
-using foo::GetReply;
-using foo::GetRequest;
-using foo::SetReply;
-using foo::SetRequest;
 using grpc::Channel;
 using grpc::ClientContext;
 using grpc::Status;
 
-class FooClient {
-   public:
-    FooClient(std::shared_ptr<Channel> channel) : stub(Foo::NewStub(channel)) {}
+class Client {
+  public:
+    Client(std::shared_ptr<Channel> channel)
+        : stub(caspaxos::Server::NewStub(channel)) {}
 
-    void Set(const int32_t value) {
-        SetRequest request;
-        request.set_value(value);
-        SetReply reply;
+    void Change() {
         ClientContext context;
-        Status status = stub->Set(&context, request, &reply);
-        if (!status.ok()) {
-            std::cout << status.error_code() << ": " << status.error_message()
-                      << std::endl;
-        }
+        caspaxos::ChangeRequest request;
+        caspaxos::ChangeResponse response;
+        std::mutex mu;
+        std::condition_variable cv;
+        bool done = false;
+        stub->async()->Change(&context, &request, &response,
+                              [&mu, &cv, &done, this](Status status) {
+                                  bool ret;
+                                  if (!status.ok()) {
+                                      LOG(INFO) << "Change rpc failed.";
+                                  } else {
+                                      LOG(INFO) << "Change rpc ok.";
+                                  }
+                                  std::lock_guard<std::mutex> lock(mu);
+                                  done = true;
+                                  cv.notify_one();
+                              });
+        std::unique_lock<std::mutex> lock(mu);
+        cv.wait(lock, [&done] { return done; });
     }
 
-    int32_t Get() {
-        GetRequest request;
-        GetReply reply;
-        ClientContext context;
-        Status status = stub->Get(&context, request, &reply);
-        if (status.ok()) {
-            return reply.value();
-        } else {
-            std::cout << status.error_code() << ": " << status.error_message()
-                      << std::endl;
-            return -1;
-        }
-    }
-
-   private:
-    std::unique_ptr<Foo::Stub> stub;
+  private:
+    std::unique_ptr<caspaxos::Server::Stub> stub;
 };
 
 int main(int argc, char* argv[]) {
     absl::ParseCommandLine(argc, argv);
+    absl::SetStderrThreshold(absl::LogSeverity::kInfo);
+    absl::InitializeLog();
     std::string target_str = absl::GetFlag(FLAGS_target);
 
-    FooClient client(
+    Client client(
         grpc::CreateChannel(target_str, grpc::InsecureChannelCredentials()));
 
-    if (absl::GetFlag(FLAGS_value).has_value()) {
-        client.Set(absl::GetFlag(FLAGS_value).value());
-    }
-
-    std::cout << target_str << " returned: " << client.Get() << std::endl;
+    client.Change();
 
     return 0;
 }
--- modules/replication/src/server/main.cc
+++ modules/replication/src/server/main.cc
@@ -1,43 +1,104 @@
 #include <iostream>
 #include <string>
 
+#include "absl/base/log_severity.h"
 #include "absl/flags/flag.h"
 #include "absl/flags/parse.h"
 #include "absl/flags/usage.h"
+#include "absl/log/check.h"
+#include "absl/log/globals.h"
+#include "absl/log/initialize.h"
+#include "absl/log/log.h"
 #include "grpc++/grpc++.h"
-#include "modules/replication/proto/foo.grpc.pb.h"
+#include "modules/replication/proto/caspaxos.grpc.pb.h"
 
 ABSL_FLAG(uint16_t, port, 50051, "Server port for the service");
 
 int32_t state;
 
-using foo::Foo;
-using foo::GetReply;
-using foo::GetRequest;
-using foo::SetReply;
-using foo::SetRequest;
+using grpc::CallbackServerContext;
 using grpc::Server;
 using grpc::ServerBuilder;
 using grpc::ServerContext;
 using grpc::Status;
 
-class FooServiceImpl final : public Foo::Service {
-    Status Get(ServerContext* context, const GetRequest* request,
-               GetReply* reply) override {
-        reply->set_value(state);
-        return Status::OK;
+class ServerImpl final : public caspaxos::Server::CallbackService {
+    grpc::ServerUnaryReactor* Change(
+        CallbackServerContext* context, const caspaxos::ChangeRequest* request,
+        caspaxos::ChangeResponse* response) override {
+        class Reactor : public grpc::ServerUnaryReactor {
+            // NOTE The callback methods (e.g., OnDone()) are supposed to return
+            // quickly. Never perform blocking work (e.g., waiting for an event)
+            // in such callbacks.
+          public:
+            Reactor(const caspaxos::ChangeRequest& point) {
+                Finish(grpc::Status::OK);
+            }
+
+          private:
+            void OnDone() override {
+                LOG(INFO) << "Change done.";
+                delete this;
+            }
+
+            void OnCancel() override { LOG(ERROR) << "Change cancelled"; }
+        };
+
+        return new Reactor(*request);
     }
-    Status Set(ServerContext* context, const SetRequest* request,
-               SetReply* reply) override {
-        state = request->value();
-        reply->set_successful(true);
-        return Status::OK;
+
+    grpc::ServerUnaryReactor* Prepare(
+        CallbackServerContext* context, const caspaxos::PrepareRequest* request,
+        caspaxos::PrepareResponse* response) override {
+        class Reactor : public grpc::ServerUnaryReactor {
+            // NOTE The callback methods (e.g., OnDone()) are supposed to return
+            // quickly. Never perform blocking work (e.g., waiting for an event)
+            // in such callbacks.
+          public:
+            Reactor(const caspaxos::PrepareRequest& point) {
+                Finish(grpc::Status::OK);
+            }
+
+          private:
+            void OnDone() override {
+                LOG(INFO) << "Prepare done.";
+                delete this;
+            }
+
+            void OnCancel() override { LOG(ERROR) << "Prepare cancelled."; }
+        };
+
+        return new Reactor(*request);
+    }
+
+    grpc::ServerUnaryReactor* Accept(
+        CallbackServerContext* context, const caspaxos::AcceptRequest* request,
+        caspaxos::AcceptResponse* response) override {
+        class Reactor : public grpc::ServerUnaryReactor {
+            // NOTE The callback methods (e.g., OnDone()) are supposed to return
+            // quickly. Never perform blocking work (e.g., waiting for an event)
+            // in such callbacks.
+          public:
+            Reactor(const caspaxos::AcceptRequest& point) {
+                Finish(grpc::Status::OK);
+            }
+
+          private:
+            void OnDone() override {
+                LOG(INFO) << "Accept done.";
+                delete this;
+            }
+
+            void OnCancel() override { LOG(ERROR) << "Accept cancelled."; }
+        };
+
+        return new Reactor(*request);
     }
 };
 
 void RunServer(uint16_t port) {
     std::string server_address = absl::StrFormat("0.0.0.0:%d", port);
-    FooServiceImpl service;
+    ServerImpl service;
 
     ServerBuilder builder;
     // Listen on the given address without any authentication mechanism.
@@ -56,6 +117,9 @@ void RunServer(uint16_t port) {
 
 int main(int argc, char* argv[]) {
     absl::ParseCommandLine(argc, argv);
+    absl::SetStderrThreshold(absl::LogSeverity::kInfo);
+    absl::InitializeLog();
+
     RunServer(absl::GetFlag(FLAGS_port));
     return 0;
 }
