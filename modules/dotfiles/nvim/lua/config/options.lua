e560742bf7c66dc37e28c30bd946aa1ba1c49d19
Sean Lyons <lyons.sean5@gmail.com>
2025-12-28 11:55:10 -0500

    Follow LazyVim starter

---
+++ modules/dotfiles/nvim/lua/config/options.lua
@@ -0,0 +1,10 @@
+vim.opt.clipboard = "unnamedplus" -- use system keyboard for yank
+
+vim.opt.mouse = ""
+
+vim.opt.tabstop = 4 -- Number of spaces a tab character represents
+vim.opt.expandtab = true -- Convert tabs to spaces
+vim.opt.shiftwidth = 4 -- Number of spaces used for indentation (e.g., when using '>>' or '<<')
+vim.opt.softtabstop = 4 -- Number of spaces a tab key press inserts in Insert mode
+vim.opt.autoindent = true -- Copy indent from previous line
+vim.opt.smartindent = true -- Smartly indent according to filetype
--- modules/dotfiles/nvim/lua/remap.lua
+++
@@ -1,8 +0,0 @@
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
-vim.keymap.set('n', '<leader>fg', builtin.git_files, { desc = 'Telescope fit files' })
-vim.keymap.set('n', '<leader>fs', function()
-	builtin.grep_string({ search = vim.fn.input("Grep > ") });
-end)
-
-vim.api.nvim_set_keymap('t', '<Esc>', '<C-\\><C-n>', { noremap = true, silent = true })
